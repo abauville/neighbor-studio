dd827716958450da805b16b02a67bf867fd060f5
Arthur Bauville <a.bauville@gmail.com>
2022-02-21 18:13:32 +0900

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_21_080102) do
+ActiveRecord::Schema.define(version: 2022_02_21_090435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "studios", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.text "descript"
+    t.bigint "user_id", null: false
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_studios_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -28,4 +39,5 @@ ActiveRecord::Schema.define(version: 2022_02_21_080102) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "studios", "users"
 end
