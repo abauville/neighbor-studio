4b70313038b0aff145d2ec11c96e1398f0cbcefc
Kanae Maki <m14ky.gu@gmail.com>
2022-02-22 17:32:45 +0900

    created private method

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,8 +1,15 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.all
+    end
+  end
+
+
+  private
+
+  def user_is_owner?
+    user == record.user
   end
 end
