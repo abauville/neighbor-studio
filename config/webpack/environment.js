005be27300d2c29030162dd741f7a17344c7dafd
nairdesigns <nairbdg@gmail.com>
2022-02-23 11:55:51 +0900

    temporary fix for javascript:

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,6 +3,3 @@ const { environment } = require("@rails/webpacker");
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete("nodeModules");
 module.exports = environment;
-
-MAPBOX_API_KEY =
-  "pk.eyJ1IjoibmFpcmRlc2lnbnMiLCJhIjoiY2t6N3oxZGtyMWY5ejJ2cGhtc283bWtoYiJ9.2Xm0lvF_0guXITdMgqokVg";
