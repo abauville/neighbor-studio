470d66c3cd2a579744d0ea9dc41024ac6f5482fb
nairdesigns <nairbdg@gmail.com>
2022-02-23 11:43:48 +0900

    map api working

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,8 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
 
 // Preventing Babel from transpiling NodeModules packages
-environment.loaders.delete('nodeModules');
-module.exports = environment
+environment.loaders.delete("nodeModules");
+module.exports = environment;
+
+MAPBOX_API_KEY =
+  "pk.eyJ1IjoibmFpcmRlc2lnbnMiLCJhIjoiY2t6N3oxZGtyMWY5ejJ2cGhtc283bWtoYiJ9.2Xm0lvF_0guXITdMgqokVg";
