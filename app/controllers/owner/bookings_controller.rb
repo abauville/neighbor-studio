1881139ecce5f42dfcd8e89e23df14f062b1ac10
Markie Choi <markie054@gmail.com>
2022-02-24 17:13:17 +0900

    updated booking controller to filter different status

--- app/controllers/owner/bookings_controller.rb
+++ app/controllers/owner/bookings_controller.rb
@@ -1,7 +1,20 @@
 class Owner::BookingsController < ApplicationController
   def index
     # @bookings = Booking.all
-    @bookings = policy_scope([:owner, Booking])
-    # render "bookings/index"
+    bookings = policy_scope([:owner, Booking])
+    ## returns array of hash with keys studio_name and studio_bookings filtered by status
+    @bookings = bookings.map(&:studio).uniq.map do |studio|
+      { studio_name: studio.name, studio_bookings: studio.bookings }
+    end
+    @pending_bookings = bookings.map(&:studio).uniq.map do |studio|
+      { studio_name: studio.name, studio_bookings: studio.bookings.pending }
+    end
+    @accepted_bookings = bookings.map(&:studio).uniq.map do |studio|
+      { studio_name: studio.name, studio_bookings: studio.bookings.accepted }
+    end
+    @refused_bookings = bookings.map(&:studio).uniq.map do |studio|
+      { studio_name: studio.name, studio_bookings: studio.bookings.refused }
+    end
+    raise
   end
 end
