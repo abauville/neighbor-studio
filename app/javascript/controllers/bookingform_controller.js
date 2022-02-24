7f38d5c9d283479c40d5e10e7ab00e7fe1d2719d
Arthur Bauville <a.bauville@gmail.com>
2022-02-24 22:02:30 +0900

    added default value to the date

--- app/javascript/controllers/bookingform_controller.js
+++ app/javascript/controllers/bookingform_controller.js
@@ -5,8 +5,12 @@ export default class extends Controller {
   static values = { price: Number }
 
   connect() {
-    console.log("bookingform_controller connected!");
     this.price_multiplier = 0;
+    const dateElement = document.querySelector("#booking_start_date");
+    console.log();
+    const today = new Date();
+    const dateString = `${today.getFullYear()}-${this.#zeroPad(today.getMonth()+1, 10)}-${this.#zeroPad(today.getDay(), 10)}`
+    dateElement.value = dateString;
   }
   updatePrice(event) {
     if (event.currentTarget.checked) {
@@ -17,5 +21,10 @@ export default class extends Controller {
 
     console.log(this.price_multiplier)
     this.submitTarget.value = `¥${this.price_multiplier * this.priceValue}`
+  }
+
+  #zeroPad(nr,base){
+    var  len = (String(base).length - String(nr).length)+1;
+    return len > 0? new Array(len).join('0')+nr : nr;
   }
 }
